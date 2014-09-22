42297b1e388546fc0d0d0f9c7d01fd3bdce789b0
Tommy Stanton <tommystanton@gmail.com>
2014-09-22 14:39:34 -0700

    Add first test: socket is opened on 5000

--- src/main/java/com/blackhawks/Server.java
+++ src/main/java/com/blackhawks/Server.java
@@ -1,9 +1,25 @@
 package com.blackhawks;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+  private static ServerSocket serverSocket;
+  private static Socket clientSocket;
+
+  public static void start() throws Exception
+  {
+    serverSocket = new ServerSocket(5000);
+  }
+
+  public static void stop() throws Exception {
+    serverSocket.close();
+  }
 
   public static void main(String[] args) throws Exception
   {
-
+    start();
+    stop();
   }
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,10 +1,19 @@
 import org.junit.*;
 
+import com.blackhawks.Server;
+
+import java.net.Socket;
+
 public class ServerTest
 {
   @Test
-  public void Todo() throws Exception
+  public void ItOpensASocketOnTheExpectedPort() throws Exception
   {
+    Server server = new Server();
+    Server.start();
 
+    Socket socket = new Socket("127.0.0.1", 5000);
+
+    Server.stop();
   }
 }
