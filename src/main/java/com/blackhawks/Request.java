bd91248a691b893d314609135c620cde7b2e7e18
Cydney Auman <cydneyredman@livenation.com>
2014-09-23 16:53:50 -0700

    Add Request handler

---
+++ src/main/java/com/blackhawks/Request.java
@@ -0,0 +1,28 @@
+package com.blackhawks;
+
+
+public class Request {
+  private String httpMethod;
+  private String url;
+  private String httpVersion;
+
+
+  public Request(String req) {
+    httpMethod = req.split("\\s+")[0];
+    url = req.split("\\s+")[1];
+    httpVersion = req.split("\\s+")[2];
+
+  }
+
+  public String getHttpMethod() {
+    return httpMethod;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public String getHttpVersion() {
+    return httpVersion;
+  }
+}
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,7 +1,9 @@
+import com.blackhawks.Request;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 import com.blackhawks.Server;
+
 
 public class ServerTest {
 
@@ -14,4 +16,16 @@ public class ServerTest {
 
     assertTrue(mocket.Started);
   }
+
+  @Test
+  public void ItParsesRequest() throws Exception {
+    String req = "GET / HTTP/1.1";
+    Request request = new Request(req);
+
+    assertEquals("GET", request.getHttpMethod());
+    assertEquals("/", request.getUrl());
+    assertEquals("HTTP/1.1", request.getHttpVersion());
+  }
+
+
 }
