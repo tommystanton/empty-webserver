9b09689b7231b23a6645601e3ce89b6164e7d659
Tommy Stanton <tommystanton@gmail.com>
2014-09-23 14:45:53 -0700

    Revert "Test: Refactor to use @Before/@After"
    This reverts commit 194d547c511f744c285f554c8e172c1b174c0e12.

--- src/test/java/InternetSocketTest.java
+++ src/test/java/InternetSocketTest.java
@@ -1,9 +1,8 @@
-import org.junit.*;
 import com.blackhawks.*;
 import com.blackhawks.Socket;
+import org.junit.Test;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
@@ -12,16 +11,12 @@ import static org.junit.Assert.assertEquals;
 
 public class InternetSocketTest {
 
-  private InetAddress host;
-  private EchoHandler handler;
-  private Socket socket;
-  private java.net.Socket client;
-
-  @Before
-  public void setup() throws IOException {
-    host = InetAddress.getLocalHost();
-    handler = new EchoHandler();
-    socket = new InternetSocket(handler);
+  @Test
+  public void StartsTheSocket() throws Exception
+  {
+    InetAddress host = InetAddress.getLocalHost();
+    EchoHandler handler = new EchoHandler();
+    final Socket socket = new InternetSocket(handler);
 
     new Thread() {
       public void run() {
@@ -29,23 +24,20 @@ public class InternetSocketTest {
       }
     }.start();
 
-    client = new java.net.Socket(host.getHostName(), 5000);
-  }
+    java.net.Socket client = new java.net.Socket(host.getHostName(), 5000);
 
-  @After
-  public void done() throws IOException {
-    client.close();
-  }
+    try {
+      PrintWriter out = new PrintWriter(client.getOutputStream());
+      BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-  @Test
-  public void StartsTheSocket() throws Exception
-  {
-    PrintWriter out = new PrintWriter(client.getOutputStream());
-    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      out.println("Test\n");
+      out.flush();
 
-    out.println("Test\n");
-    out.flush();
+      assertEquals("Test", in.readLine());
+    }
+    finally {
+      client.close();
+    }
 
-    assertEquals("Test", in.readLine());
   }
 }
