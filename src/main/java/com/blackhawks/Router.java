9ac5f500924ae6e63ccd0ac4cfd6b1ff3cbb3290
Tommy Stanton <tommystanton@gmail.com>
2014-09-29 15:39:05 -0700

    Router: Store route existence in a HashMap

--- src/main/java/com/blackhawks/Router.java
+++ src/main/java/com/blackhawks/Router.java
@@ -1,10 +1,25 @@
 package com.blackhawks;
 
+import java.util.HashMap;
+
 public class Router {
 
-    public boolean isResourceExistent(String resource){
-        if(resource.equals("/") || resource.equals("/form")) {
-            return true;
+    private HashMap<String, Boolean> routes;
+
+    public Router() {
+        this.routes = new HashMap<>();
+        setRoutes();
+    }
+
+    private void setRoutes() {
+        routes.put("/", true);
+        routes.put("/form", true);
+    }
+
+    public boolean isResourceExistent(String resource) {
+
+        if(routes.containsKey(resource)) {
+            return routes.get(resource);
         }
 
         return false;
